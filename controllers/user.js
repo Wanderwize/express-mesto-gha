555b69ba6cb312fd04895ec40fe64e57964c631f
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-06 15:05:32 +0500

    sprint 13 5 commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,15 @@
 const User = require('../models/user');
 
-module.exports.getUser = (req, res) => {
-  User.findById(req.user._id)
+module.exports.getUser = (req, res, next) => {
+  const { userId } = req.params;
+  User.findById(userId)
     .then((user) => res.send({ data: user, message: 'Все ок' }))
 
     .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(404).send({ message: 'Нет пользователя с таким id' });
       }
+
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
@@ -19,9 +21,17 @@ module.exports.getUsers = (req, res) => {
       if (err.status === 'CastError') {
         return res.status(404).send({ message: 'Пользователи отсутствуют' });
       }
+
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
+
+// module.exports.getUsers = (req, res, next) => {
+//   User.find({})
+//     .then((users) => res.send(users))
+//     .catch(() => next(new NotFoundError('не нашлос')))
+//     .catch(() => next(new DefaultError('error')));
+// };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
@@ -37,12 +47,17 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send(user))
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true, upsert: true },
+  )
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'Ошибка валидации' });
-      } if (err.name === 'CastError') {
+      }
+      if (err.name === 'CastError') {
         return res.status(404).send({ message: 'Нет пользователя с таким id' });
       }
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
@@ -52,12 +67,17 @@ module.exports.updateProfile = (req, res) => {
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true, upsert: true },
+  )
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidatorError') {
+      if (err.name === 'ValidationError') {
         return res.status(400).send({ message: 'Ошибка валидации' });
-      } if (err.name === 'CastError') {
+      }
+      if (err.name === 'CastError') {
         return res.status(404).send({ message: 'Нет пользователя с таким id' });
       }
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
