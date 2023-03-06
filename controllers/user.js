b4039c6be74bbccb8b3844ca88640e55ce573135
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-06 21:45:37 +0500

    sprint 13 9 commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,37 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-module.exports.getUser = (req, res, next) => {
+module.exports.getUser = (req, res) => {
   const { userId } = req.params;
+  if (userId.length === 24) {
+    User.findById(userId)
+      .then((user) => res.send(user))
+      .catch((err) => {
+        if (err.name === "CastError") {
+          return res
+            .status(404)
+            .send({ message: "Нет пользователя с таким id" });
+        }
+        if (err.name === "ValidationError") {
+          return res.status(400).send({ message: "Ошибка валидации" });
+        }
 
-  User.findById(req.params.userId, { runValidators: true })
-    .then((user) => res.send({ data: user, message: 'Все ок' }))
-
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации' });
-      }
-
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
-    });
+        return res.status(500).send({ message: "На сервере произошла ошибка" });
+      });
+  } else return res.status(400).send({ message: "Некорректный формат данных" });
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
     .catch((err) => {
-      if (err.status === 'CastError') {
-        return res.status(400).send({ message: 'Пользователи отсутствуют' });
+      if (err.status === "CastError") {
+        return res.status(400).send({ message: "Пользователи отсутствуют" });
       }
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации' });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
       }
 
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+      return res.status(500).send({ message: "На сервере произошла ошибка" });
     });
 };
 
@@ -38,31 +40,32 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации' });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
       }
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+      return res.status(500).send({ message: "На сервере произошла ошибка" });
     });
 };
 
-module.exports.errorPage = (req, res) => res.status(404).send({ message: 'Такой страницы не существует' });
+module.exports.errorPage = (req, res) =>
+  res.status(404).send({ message: "Такой страницы не существует" });
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true, runValidators: true, upsert: true },
+    { new: true, runValidators: true, upsert: true }
   )
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации' });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
       }
-      if (err.name === 'CastError') {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      if (err.name === "CastError") {
+        return res.status(404).send({ message: "Нет пользователя с таким id" });
       }
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+      return res.status(500).send({ message: "На сервере произошла ошибка" });
     });
 };
 
@@ -72,16 +75,16 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
-    { new: true, runValidators: true, upsert: true },
+    { new: true, runValidators: true, upsert: true }
   )
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации' });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
       }
-      if (err.name === 'CastError') {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      if (err.name === "CastError") {
+        return res.status(404).send({ message: "Нет пользователя с таким id" });
       }
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+      return res.status(500).send({ message: "На сервере произошла ошибка" });
     });
 };
