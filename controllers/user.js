607a91fca75035cb41e6e664c275c99baf8354c9
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-17 21:32:58 +0500

    sprint 14.7

--- controllers/user.js
+++ controllers/user.js
@@ -115,13 +115,13 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      if (!user) {
+        throw new AuthorizationError('Ошибка авторизации');
+      }
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
         expiresIn: '7d',
       });
       res.send({ token });
     })
-    .catch((err) => {
-      console.log(err);
-      throw new AuthorizationError('Ошибка авторизации');
-    });
+    .catch(next);
 };
