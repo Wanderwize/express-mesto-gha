b14d63a7e0fed4a421d3b9a2b9840dbffe74ed3e
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-18 19:14:54 +0500

    sprint 14.12

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const path = require('path');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const { celebrate, Joi } = require('celebrate');
-const userRouter = require('./routes/user');
-const { login, createUser } = require('./controllers/user');
-const cardRouter = require('./routes/card');
+const path = require("path");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const { celebrate, Joi } = require("celebrate");
+const userRouter = require("./routes/user");
+const { login, createUser } = require("./controllers/user");
+const cardRouter = require("./routes/card");
 // const auth = require('./middlewares/auth');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
+mongoose.connect("mongodb://0.0.0.0:27017/mestodb", {
   useNewUrlParser: true,
 
   useUnifiedTopology: true,
@@ -28,27 +28,43 @@ app.use(express.json());
 app.use(userRouter);
 app.use(cardRouter);
 
-app.post('/signin', login);
 app.post(
-  '/signup',
+  "/signin",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
+      avatar: Joi.string().pattern(
+        /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/
+      ),
     }),
   }),
-  createUser,
+  login
+);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/
+      ),
+    }),
+  }),
+  createUser
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(errors());
 
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: 'Произошла ошибка' });
+  res.status(500).send({ message: "Произошла ошибка" });
 });
 
 app.listen(PORT, () => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,9 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const NotFoundError = require('../errors/notFoundError');
-const DefaultError = require('../errors/defaultError');
-const ValidationError = require('../errors/validationError');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const NotFoundError = require("../errors/notFoundError");
+const DefaultError = require("../errors/defaultError");
+const ValidationError = require("../errors/validationError");
 // const AuthorizationError = require('../errors/authoriztionError');
 
 module.exports.getUser = (req, res, next) => {
@@ -12,40 +12,40 @@ module.exports.getUser = (req, res, next) => {
     User.findById(userId)
       .then((user, err) => {
         if (!user) {
-          throw new NotFoundError('Нет пользователя с таким id');
+          throw new NotFoundError("Нет пользователя с таким id");
         }
-        if (err.name === 'CastError') {
-          throw new NotFoundError('Нет пользователя с таким id');
+        if (err.name === "CastError") {
+          throw new NotFoundError("Нет пользователя с таким id");
         }
-        if (err.name === 'ValidationError') {
-          throw new ValidationError('Ошибка валидации');
+        if (err.name === "ValidationError") {
+          throw new ValidationError("Ошибка валидации");
         }
         return res.send({ data: user });
       })
       .catch(next);
   }
 
-  return console.log('test');
+  return console.log("test");
 };
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
     .catch((err) => {
-      if (err.status === 'CastError') {
-        throw new NotFoundError('Нет пользователя с таким id');
+      if (err.status === "CastError") {
+        throw new NotFoundError("Нет пользователя с таким id");
       }
-      if (err.name === 'ValidationError') {
-        throw new ValidationError('Ошибка валидации');
+      if (err.name === "ValidationError") {
+        throw new ValidationError("Ошибка валидации");
       }
 
-      throw new DefaultError('На сервере произошла ошибка');
+      throw new DefaultError("На сервере произошла ошибка");
     });
 };
 
 module.exports.errorPage = (req, res, next) => {
   console.log(req, res);
-  throw new NotFoundError('Такой страницы не существует');
+  throw new NotFoundError("Такой страницы не существует");
 };
 
 module.exports.updateProfile = (req, res) => {
@@ -53,17 +53,17 @@ module.exports.updateProfile = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
-    { new: true, runValidators: true, upsert: true },
+    { new: true, runValidators: true, upsert: true }
   )
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new ValidationError('Ошибка валидации');
+      if (err.name === "ValidationError") {
+        throw new ValidationError("Ошибка валидации");
       }
-      if (err.name === 'CastError') {
-        throw new NotFoundError('Нет пользователя с таким id');
+      if (err.name === "CastError") {
+        throw new NotFoundError("Нет пользователя с таким id");
       }
-      throw new DefaultError('На сервере произошла ошибка');
+      throw new DefaultError("На сервере произошла ошибка");
     });
 };
 
@@ -73,39 +73,44 @@ module.exports.updateAvatar = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
-    { new: true, runValidators: true, upsert: true },
+    { new: true, runValidators: true, upsert: true }
   )
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new ValidationError('Ошибка валидации');
+      if (err.name === "ValidationError") {
+        throw new ValidationError("Ошибка валидации");
       }
-      if (err.name === 'CastError') {
-        throw new NotFoundError('Нет пользователя с таким id');
+      if (err.name === "CastError") {
+        throw new NotFoundError("Нет пользователя с таким id");
       }
-      throw new DefaultError('На сервере произошла ошибка');
+      throw new DefaultError("На сервере произошла ошибка");
     });
 };
 
 module.exports.createUser = (req, res, next) => {
-  const {
-    name, about, avatar, email,
-  } = req.body;
+  const { name, about, avatar, email } = req.body;
 
   bcrypt
     .hash(req.body.password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
     .then((user) => {
       if (!user) {
-        throw new ValidationError('Ошибка валидации');
+        throw new ValidationError("Ошибка валидации");
       }
-      res.send({ data: user });
+      res.send({
+        email: user.email,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+      });
     })
     .catch(next);
 };
@@ -115,8 +120,11 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
-        expiresIn: '7d',
+      if (!user) {
+        throw new ValidationError("Ошибка валидации");
+      }
+      const token = jwt.sign({ _id: user._id }, "some-secret-key", {
+        expiresIn: "7d",
       });
       res.send({ token });
     })
