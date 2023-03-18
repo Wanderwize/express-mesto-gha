4449061fd9dfaced11c21fc84597a2b59178d24e
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-18 18:51:09 +0500

    sprint 14.11

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
-      name: Joi.string().required().min(2).max(30),
+      name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri(),
+      avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
     }),
   }),
   createUser,
