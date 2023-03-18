6ef4bb1959d2b9150fe1c29e43c258b226457b8d
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-18 22:01:57 +0500

    sprint 14.22

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ userRouter.patch(
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
       name: Joi.string().required().min(2).max(30),
-      about: Joi.string().min(2).max(30),
+      about: Joi.string().min(2),
     }),
   }),
   updateProfile
