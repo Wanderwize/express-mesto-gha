293b2ad63d4c3c4f4f0f34af096b5ec06cd5ca9a
Wanderwize~ <wanderwwwize@yandex.ru>
2023-03-19 19:21:18 +0500

    sprint 14.28

--- controllers/card.js
+++ controllers/card.js
@@ -1,38 +1,84 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
+
+// module.exports.deleteCard = (req, res) => {
+//   const { cardId } = req.params;
+//   if (cardId.length === 24 && req.params.card.owner._id === req.user._id) {
+//     Card.findByIdAndRemove(cardId, {
+//       new: true,
+//       runValidators: true,
+//       upsert: true,
+//     })
+//       .then((card) => {
+//         if (!card) {
+//           return res.status(404).send({ message: "Карточка не найдена" });
+//         }
+//         return res.send({ data: cardId });
+//       })
+//       .catch((err) => {
+//         if (err.name === "CastError") {
+//           return res.status(404).send({ message: "Нет такой карточки" });
+//         }
+//         return res.status(500).send({ message: "На сервере произошла ошибка" });
+//       });
+//   } else return res.status(400).send({ message: "Некорректный формат данных" });
+//   return console.log("test");
+// };
 
 module.exports.deleteCard = (req, res) => {
-  const { cardId, owner, user } = req.params;
-  if (cardId.length === 24 && owner._id === user._id) {
-    Card.findByIdAndRemove(cardId, {
-      new: true,
-      runValidators: true,
-      upsert: true,
-    })
-      .then((card) => {
-        if (!card) {
-          return res.status(404).send({ message: 'Карточка не найдена' });
-        }
-        return res.send({ data: cardId });
+  const card = Card.findById(req.params.id);
+  if (card.owner._id === req.user._id) {
+    const { cardId } = req.params;
+    if (cardId.length === 24) {
+      Card.findByIdAndRemove(cardId, {
+        new: true,
+        runValidators: true,
+        upsert: true,
       })
-      .catch((err) => {
-        if (err.name === 'CastError') {
-          return res.status(404).send({ message: 'Нет такой карточки' });
-        }
-        return res.status(500).send({ message: 'На сервере произошла ошибка' });
-      });
-  } else return res.status(400).send({ message: 'Некорректный формат данных' });
-  return console.log('test');
+        .then((card) => {
+          if (!card) {
+            return res.status(404).send({ message: "Карточка не найдена" });
+          }
+          return res.send({ data: cardId });
+        })
+        .catch((err) => {
+          if (err.name === "CastError") {
+            return res.status(404).send({ message: "Нет такой карточки" });
+          }
+          return res
+            .status(500)
+            .send({ message: "На сервере произошла ошибка" });
+        });
+    } else
+      return res.status(400).send({ message: "Некорректный формат данных" });
+  }
+  return console.log("test");
 };
+
+//   const { cardId } = req.params;
+//   if (cardId.length === 24 && req.params.card.owner._id === req.user._id) {
+//     Card.findByIdAndRemove(cardId, {
+//       new: true,
+//       runValidators: true,
+//       upsert: true,
+//     }).then((card) => {
+//       if (!card) {
+//         return res.status(404).send({ message: "Карточка не найдена" });
+//       }
+//       return res.send({ data: cardId });
+//     });
+//   }
+//   return console.log("test");
+// };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Ошибка валидации' });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
       }
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+      return res.status(500).send({ message: "На сервере произошла ошибка" });
     });
 };
 
@@ -40,10 +86,10 @@ module.exports.getCards = (req, res) => {
   Card.find({})
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(404).send({ message: 'Карточки отсутствуют' });
+      if (err.name === "CastError") {
+        return res.status(404).send({ message: "Карточки отсутствуют" });
       }
-      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+      return res.status(500).send({ message: "На сервере произошла ошибка" });
     });
 };
 
@@ -54,23 +100,23 @@ module.exports.likeCard = (req, res) => {
       cardId,
       { $addToSet: { likes: cardId } },
       { new: true },
-      { runValidators: true },
+      { runValidators: true }
     )
       .then((card) => {
         if (!card) {
-          return res.status(404).send({ message: 'Карточка не найдена' });
+          return res.status(404).send({ message: "Карточка не найдена" });
         }
         return res.send({ data: cardId });
       })
       .catch((err) => {
-        if (err.name === 'CastError') {
-          return res.status(404).send({ message: 'Нет такой карточки' });
+        if (err.name === "CastError") {
+          return res.status(404).send({ message: "Нет такой карточки" });
         }
 
-        return res.status(500).send({ message: 'На сервере произошла ошибка' });
+        return res.status(500).send({ message: "На сервере произошла ошибка" });
       });
-  } else return res.status(400).send({ message: 'Некорректный формат данных' });
-  return console.log('test');
+  } else return res.status(400).send({ message: "Некорректный формат данных" });
+  return console.log("test");
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -80,20 +126,20 @@ module.exports.dislikeCard = (req, res) => {
       cardId,
       { $pull: { likes: cardId } },
       { new: true },
-      { new: true, runValidators: true, upsert: true },
+      { new: true, runValidators: true, upsert: true }
     )
       .then((card) => {
         if (!card) {
-          return res.status(404).send({ message: 'Карточка не найдена' });
+          return res.status(404).send({ message: "Карточка не найдена" });
         }
         return res.send({ data: cardId });
       })
       .catch((err) => {
-        if (err.name === 'CastError') {
-          return res.status(404).send({ message: 'Нет такой карточки' });
+        if (err.name === "CastError") {
+          return res.status(404).send({ message: "Нет такой карточки" });
         }
-        return res.status(500).send({ message: 'На сервере произошла ошибка' });
+        return res.status(500).send({ message: "На сервере произошла ошибка" });
       });
-  } else return res.status(400).send({ message: 'Некорректный формат данных' });
-  return console.log('test');
+  } else return res.status(400).send({ message: "Некорректный формат данных" });
+  return console.log("test");
 };
